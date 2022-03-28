c9e486d2bf0da400a14093c02eaf31a990f83567
Rustam Nassimi <nassimi.exceedteam@gmail.com>
2022-03-28 12:15:24 +0300

    Edited 'Edit button'

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ let allTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 let valueInput = '';
 let input = null;
 
+
 window.onload = async function init() {
   input = document.getElementById('add-task');
   input.addEventListener('change', updateValue);
+  getTasks();
+}
+
+const getTasks = async () => {
   const response = await fetch('http://localhost:8000/allTasks', {
     method: 'GET'
   });
@@ -15,30 +20,30 @@ window.onload = async function init() {
 
 onClickButton = async () => {
   if (valueInput.length > 0) {
-    allTasks.push({
-      text: valueInput,
-      isCheck: false,
-    });
-    const response = await fetch('http://localhost:8000/createTask', {
-      method: 'POST',
+    pushTasks();
+  } else {
+    alert('Input something!')
+  }
+}
+
+const pushTasks = async () => {
+  const response = await fetch('http://localhost:8000/createTask', {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
       text: valueInput,
-      isCheck: false,
-    })
+      isCheck: false
+    }),
   });
-  let result = await response.json();
-  allTasks = result.data;
-    valueInput = '';
-    input.value = '';
-    localStorage.setItem('tasks', JSON.stringify(allTasks));
+  const resultPOST = await response.json().then((response) => {
+    allTasks.push(response);
+    input.value = "";
+    valueInput = "";
     render();
-  } else {
-    alert('Input something!')
-  }
+  })
 }
 
 updateValue = (event) => {
@@ -50,69 +55,81 @@ render = () => {
   while (content.firstChild) {
     content.removeChild(content.firstChild);
   }
+  allTasks
+    .sort((a, b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0)
+    .map(item => {
+      const { _id, isCheck, text } = item;
+      const container = document.createElement('div');
+      container.id = `task-${_id}`;
+      container.className = 'task-container';
 
-  allTasks.sort((a, b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0);
-  allTasks.map((item, index) => {
-    const container = document.createElement('div');
-    container.id = `task-${index}`;
-    container.className = 'task-container';
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.checked = isCheck;
+      checkbox.onclick = function (e) {
+        onChangeCheckbox(e, _id);
+      }
+      container.appendChild(checkbox);
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = item.isCheck;
-    checkbox.onchange = function () {
-      onChangeCheckbox(index);
-    }
-    container.appendChild(checkbox);
+      const texts = document.createElement('p');
+      texts.innerText = text;
+      texts.id = `task-text-${_id}`;
+      texts.className = isCheck ? 'text-task done-text' : 'text-task';
+      container.appendChild(texts);
 
-    const text = document.createElement('p');
-    text.innerText = item.text;
-    text.className = item.isCheck ? 'text-task done-text' : 'text-task';
-    container.appendChild(text);
+      const imageEdit = document.createElement('img');
+      imageEdit.src = "https://icons.veryicon.com/png/o/internet--web/billion-square-cloud/rename-5.png";
+      imageEdit.onclick = () => {
+        editTask(item, id, container);
+      }
+      container.appendChild(imageEdit);
 
-    const imageEdit = document.createElement('img');
-    imageEdit.src = "https://icons.veryicon.com/png/o/internet--web/billion-square-cloud/rename-5.png";
-    imageEdit.onclick = () => {
-      editTask(item, index, container);
-    }
-    container.appendChild(imageEdit);
-
-    const imageDelete = document.createElement('img');
-    imageDelete.src = "https://www.pngplay.com/wp-content/uploads/7/Delete-Icon-Background-PNG-Image.png";
-    imageDelete.onclick = () => {
-      deleteTask(index);
-    }
-    container.appendChild(imageDelete);
-    content.appendChild(container);
-  })
+      const imageDelete = document.createElement('img');
+      imageDelete.src = "https://www.pngplay.com/wp-content/uploads/7/Delete-Icon-Background-PNG-Image.png";
+      imageDelete.onclick = () => {
+        deleteTask(item._id);
+      }
+      container.appendChild(imageDelete);
+      content.appendChild(container);
+    })
 }
 
-const onChangeCheckbox = async (index) => {
-  allTasks[index].isCheck = !allTasks[index].isCheck;
-  localStorage.setItem('tasks', JSON.stringify(allTasks));
+const onChangeCheckbox = async (e, _id) => {
+  const response = await fetch('http://localhost:8000/updateTask', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      _id,
+      isCheck: e.target.checked
+    })
+  });
+  const result = await response.json();
+  allTasks = result;
   render();
 }
 
 const updateTaskText = (event) => {
   allTasks[activeEditTask].text = event.target.value;
-  localStorage.setItem('tasks', JSON.stringify(allTasks));
   render();
 }
 
-const editTask = async (item, index, container) => {
+const editTask = (container, _id, item) => {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
   const editInput = document.createElement('input');
   editInput.type = 'text';
   editInput.value = item.text;
-  editInput.id = `input-${index}`;
+  editInput.id = `input-${_id}`;
   container.appendChild(editInput);
 
   const editButton = document.createElement('input');
   editButton.type = "button";
   editButton.value = "Save";
-  editButton.id = `button-${index}`;
+  editButton.id = `button-${_id}`;
 
   const cancelButton = document.createElement('input');
   cancelButton.type = "button";
@@ -122,46 +139,40 @@ const editTask = async (item, index, container) => {
   }
   container.appendChild(cancelButton);
 
-  editButton.onclick = () => { //
-    saveValue(index)
-
+  editButton.onclick = () => {
+    saveValue(_id)
   }
   container.appendChild(editButton);
-  localStorage.setItem('tasks', JSON.stringify(allTasks));
 }
 
 const cancel = async () => {
   render();
 }
 
-const saveValue = async (index) => {
-  const oldText = document.getElementById(`input-${index}`);
-  allTasks[index].text = oldText.value;
-  const response = await fetch(`http://localhost:8000/updateTask`, {
+const saveValue = async (_id) => {
+  const newText = document.getElementById(`input-${_id}`)
+  const response = await fetch('http://localhost:8000/updateTask', {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      text: valueInput,
-      isCheck: false,
+      _id: _id,
+      text: newText.value
     }),
   });
-  let result = await response.json();
-  allTasks[index].text = result.data;
-  localStorage.setItem('tasks', JSON.stringify(allTasks));
+  const result = await response.json();
+  allTasks = result;
   render();
 }
 
-const deleteTask = async (index) => {
-  allTasks.splice(index, 1);
-  const response = await fetch(`http://localhost:8000/deleteTask`, {
+const deleteTask = async (_id) => {
+  console.log(_id)
+  const response = await fetch(`http://localhost:8000/deleteTask?_id=${_id}`, {
     method: 'DELETE',
-  });
-  let result = await response.json();
-  allTasks = result.data;
-  localStorage.setItem('tasks', JSON.stringify(allTasks));
+  })
+  const resp = await response.json();
+  allTasks = resp;
   render();
 }
-
